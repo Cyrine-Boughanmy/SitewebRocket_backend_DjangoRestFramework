7cf24f3e2d3a8a2808c4ae01f6b40a73ec632028
Cyrine <boughanmycyrine@gmail.com>
2022-11-13 13:55:57 +0100

    committing rocket website backend

--- RocketSite_back/settings.py
+++ RocketSite_back/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hxw+ab0y@^=82@$qs-u$^7a!$z3%rjrxs&2+b@_^2k)rh#h%v8'
+SECRET_KEY = 'd******************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
